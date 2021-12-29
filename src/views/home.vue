118d839b21791976cba5c081d51619ffb5fbd32a
RockChang <peng0556@qq.com>
2021-12-29 17:56:16 +0800

    feat: 添加菜单

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="home">
-		<!-- <img alt="么么么哒" src="../assets/IMG_1864.jpg" /> -->
+		<img
+			alt="么么么哒"
+			src="https://raw.githubusercontent.com/RockChang0556/pic/main/chang/Xnip2021-12-29_17-51-08.png"
+		/>
 		<!-- <h3>阿畅的专属小网站, 后续更多功能建设中...</h3> -->
 		<p>
 			<router-link to="/eat"><button>今天吃什么呢</button></router-link>
@@ -23,7 +26,7 @@ export default defineComponent({
 <style lang="less">
 .home {
 	> img {
-		width: 200px;
+		width: 1000px;
 	}
 }
 </style>
